5bd1598f093ab56c9aa4b41ed8b2202d8e277836
MattBussing <mbussing@mymail.mines.edu>
2019-11-11 18:43:36 -0700

    started cleaning and added script

--- remove_bad_images.py
+++ remove_bad_images.py
@@ -1,7 +1,11 @@
 from PIL import Image
 import os
+import tkinter
+from tkinter import Tk, Canvas, PhotoImage
 import shutil
 import matplotlib.pyplot as plt
+import cv2
+
 
 files = []
 count = 0
@@ -10,6 +14,10 @@ ORIGINAL = "Images"
 OLD = 'OldImages'
 execution_path = os.getcwd()
 
+start_working = False
+last_location = ''
+if last_location == '':
+    start_working = True
 
 # copy directory tree
 # check if it exists
@@ -20,6 +28,7 @@ if OLD not in os.listdir(execution_path):
 else:
     print('file tree already created')
 
+
 # iterate through files and display them
 # iterate through each breed
 location = execution_path + '/' + ORIGINAL
@@ -29,14 +38,27 @@ for current_breed in os.listdir(location):
     print('looking at: ' + current_breed)
     image_base_location = location + '/' + current_breed
     for filename in os.listdir(image_base_location):
+
+        pair = current_breed + '/' + filename
+        print(current_breed + '/' + filename)
+        if pair == last_location:
+            print('at location beggining')
+            start_working = True
+        
         # image?
-        if filename.endswith(".jpg"):
+        if filename.endswith(".jpg") and start_working:
+            # file to work with
             image = image_base_location + '/' + filename
             # show image
-            Image.open(image).show()
+            # Image.open(image).show()
+            # img = cv2.imread(image, cv2.IMREAD_COLOR)
+            # cv2.imshow('image2', img)
+            tk = Tk()
+            canvas = Canvas(tk)
+            canvas.pack()
+            img = PhotoImage(Image.open(image))
+            canvas.create_image((0, 0), img)
 
-            print(image_base_location)
-            print()
             # potentially move
             while True:
                 change = input('move (y/n):')
@@ -44,7 +66,13 @@ for current_breed in os.listdir(location):
                     backup_loc = image_base_location.replace(
                         ORIGINAL, OLD) + '/' + filename
                     shutil.move(image, backup_loc)
+                    print('moved')
+                    break
                 elif change == 'n':
                     print('not moving')
+                    break
+                elif change == 'exit':
+                    print(pair)
+                    exit(1)
                 else:
                     print('error y or n')
